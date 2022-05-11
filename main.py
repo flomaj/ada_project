00e6693d0cc6a7de06324f80aeaf3dd4776510b5
flomaj <100148022+flomaj@users.noreply.github.com>
2022-05-11 10:48:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,48 +7,48 @@ import sklearn.metrics
 import torch 
 import torch.nn as nn
 from torch.autograd import Variable
+from sklearn.preprocessing import StandardScaler
 
 #import the data into a dataframe
 
-df = pd.read_csv('return.csv', index_col = 'date', parse_dates=True)
+df = pd.read_csv('final_data.csv', index_col = 'date', parse_dates=True)
 
 #define our inputs and output
 
-X = df.iloc[:, 0:8] #Inputs
-y = df.iloc[:, 8:9] #Output => daily return
+X = df.iloc[:, 0:7] #Inputs
+y = df.iloc[:, 7:8] #Output => daily return
 
 #Preprocess the inputs. Output can stay under their original "form".
 
-from sklearn.preprocessing import StandardScaler
+
 ss = StandardScaler()
 X = ss.fit_transform(X)
-y = y.values
 
 #define our data
 
 starting_data_number = 0
-cut_point = 1850
+cut_point = 1867
 
 #split the data into two parts (training data & test data)
 
-X_train = X[starting_data_number:cut_point, :]
-X_test = X[cut_point:, :]
+inputs_train = inputs_ss[starting_data_number:cut_point, :]
+inputs_test = inputs_ss[cut_point:, :]
 
-y_train = y[starting_data_number:cut_point, :]
-y_test = y[cut_point:, :] 
+output_train = output[starting_data_number:cut_point, :]
+output_test = output[cut_point:, :]  
 
 #convert the Numpy Arrays to Tensors and to Variables
 
-X_train_tensors = Variable(torch.Tensor(X_train)) #size : [1870, 8]
-X_test_tensors = Variable(torch.Tensor(X_test)) #size : [19, 8]
+inputs_train_tensors = Variable(torch.Tensor(inputs_train)) #size : [1870, 8]
+inputs_test_tensors = Variable(torch.Tensor(inputs_test)) #size : [19, 8]
 
-y_train_tensors = Variable(torch.Tensor(y_train))
-y_test_tensors = Variable(torch.Tensor(y_test))
+output_train_tensors = Variable(torch.Tensor(output_train))
+output_test_tensors = Variable(torch.Tensor(output_test))
 
 #reshaping to rows, timestamps, features
 
-X_train_tensors_final = torch.reshape(X_train_tensors,   (X_train_tensors.shape[0], 1, X_train_tensors.shape[1]))
-X_test_tensors_final = torch.reshape(X_test_tensors,  (X_test_tensors.shape[0], 1, X_test_tensors.shape[1]))
+inputs_train_tensors_final = torch.reshape(inputs_train_tensors,   (inputs_train_tensors.shape[0], 1, inputs_train_tensors.shape[1])) #size : [1870, 1, 8]
+inputs_test_tensors_final = torch.reshape(inputs_test_tensors,  (inputs_test_tensors.shape[0], 1, inputs_test_tensors.shape[1])) #size : [19, 1, 8]
 
 #define the LSTM
 
@@ -61,80 +61,92 @@ class LSTM1(nn.Module):
         self.hidden_size = hidden_size #hidden state
         self.seq_length = seq_length #sequence length
 
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                          num_layers=num_layers, batch_first=True) #lstm
-        self.fc_1 =  nn.Linear(hidden_size, 128) #fully connected 1
-        self.fc = nn.Linear(128, num_classes) #fully connected last layer
-
-        self.relu = nn.ReLU()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True) #lstm
+        self.lstm_bis = nn.LSTM(hidden_size, hidden_size, num_layers)
+        self.fc = nn.Linear(hidden_size, num_classes) #fully connected last layer
+        self.sigmoid = nn.Sigmoid() #sigmoid activation function
+        self.silu = nn.SiLU() #swish activation function
     
     def forward(self,x):
         h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #hidden state
         c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #internal state
+        h_1 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #hidden state
+        c_1 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) #internal state
         # Propagate input through LSTM
-        output, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
-        hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
-        out = self.relu(hn)
-        out = self.fc_1(out) #first Dense
-        out = self.relu(out) #relu
+        output, (hn, cn) = self.lstm(x, (h_0, c_0)) #1st lstm
+        output_bis, (hn1, cn1) = self.lstm_bis(hn, (h_1, c_1)) #2nd lstm
+        hn1 = hn1.view(-1, self.hidden_size) #reshaping the data for Dense layer next
+        out = self.sigmoid(hn1) #sigmoid activation function
         out = self.fc(out) #Final Output
         return out
     
 #define the parameters of the LSTM
 
-num_epochs = 5000 #10000 epochs
+num_epochs = 1000 #10000 epochs
 learning_rate = 0.01 #0.01 lr
 
-input_size = 8 #number of inputs of the model.
-hidden_size = 5 #number of features in hidden state
+input_size = 7 #number of inputs of the model.
+hidden_size = 4 #number of features in hidden state
 num_layers = 1 #number of stacked lstm layers
 
-num_classes = 1 #number of output classes 
+num_classes = 1 #size of the output
 
-#instantiate the class
+n = int(input("How many times do you want to run the model?"))
+r2_total = []
+r2_predictions = []
+predictions = pd.DataFrame()
 
-lstm1 = LSTM1(num_classes, input_size, hidden_size, num_layers, X_train_tensors_final.shape[1])
+for i in range(n):
+    #instantiate the class
 
-#loss function & optimizer
+    lstm1 = LSTM1(num_classes, input_size, hidden_size, num_layers, inputs_train_tensors_final.shape[1])
+    
+    #loss function & optimizer
 
-criterion = torch.nn.MSELoss() # We use the mean-squared error as loss function
-#criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(lstm1.parameters(), lr=learning_rate) #Between RMSProp, AdaDelta and Adam, Adam was found to slightly outperform, so we've chosen Adam. 
+    criterion = torch.nn.MSELoss() # We use the mean-squared error as loss function
 
-#train the model
+    optimizer = torch.optim.Adam(lstm1.parameters(), lr=learning_rate) #Between RMSProp, AdaDelta and Adam, Adam was found to slightly outperform, so we've chosen Adam.
+    
+    #train the model
 
-for epoch in range(num_epochs):
-    outputs = lstm1.forward(X_train_tensors_final) #forward pass
-    optimizer.zero_grad() #caluclate the gradient, manually setting to 0
- 
-    # obtain the loss function
-    loss = criterion(outputs, y_train_tensors)
- 
-    loss.backward() #calculates the loss of the loss function
- 
-    optimizer.step() #improve from loss, i.e backprop
-    if epoch % 500 == 0:
-        print("Epoch: %d, loss: %1.5f" % (epoch, loss.item())) 
+    for epoch in range(num_epochs):
+        outputs_train = lstm1.forward(inputs_train_tensors_final) #forward pass 
+        loss_train = criterion(outputs_train, output_train_tensors) # calculate the loss (in our case based on MSE)
+        #outputs_test = lstm1.forward(inputs_test_tensors_final) #forward pass 
+        #loss_test = criterion(outputs_test, output_test_tensors) # calculate the loss (in our case based on MSE)
+
+        optimizer.zero_grad() #caluclate the gradient, manually setting to 0
+        loss_train.backward() # backwards pass
+        optimizer.step() # update model parameters
         
-#bring the original dataset into the model suitable format
+    #bring the original dataset into the model suitable format
 
-df_X_ss = ss.transform(df.iloc[:, 0:8]) #old transformers
-df_X_ss = Variable(torch.Tensor(df_X_ss)) #converting to Tensors
+    df_X_ss = ss.transform(df.iloc[:, 0:7]) #old transformers
+    df_X_ss = Variable(torch.Tensor(df_X_ss)) #converting to Tensors
 
-y = Variable(torch.Tensor(y))
-#reshaping the dataset
-df_X_ss = torch.reshape(df_X_ss, (df_X_ss.shape[0], 1, df_X_ss.shape[1]))
+    y = Variable(torch.Tensor(output))
+    #reshaping the dataset
+    df_X_ss = torch.reshape(df_X_ss, (df_X_ss.shape[0], 1, df_X_ss.shape[1]))
+    
+    #make the predictions
 
-#make the predictions
+    train_predict = lstm1(df_X_ss) #forward pass
+    data_predict = train_predict.data.numpy() #numpy conversion
+    test_predic = pd.DataFrame(data_predict[cut_point:,:])
+    
+    #compute R squared
+    r2_total.append(sklearn.metrics.r2_score(y,data_predict))
+    r2_predictions.append(sklearn.metrics.r2_score(y[cut_point:,:],data_predict[cut_point:,:]))
+    
+    #compute predicted data
+    predictions = pd.concat([predictions,test_predic], axis=1)
+    
+plt.hist(r2_predictions)
+plt.show()
 
-train_predict = lstm1(df_X_ss) #forward pass
-data_predict = train_predict.data.numpy() #numpy conversion
+r2 = pd.DataFrame(r2_predictions)
+r2.describe()
 
-#compute R squared
-
-print(sklearn.metrics.r2_score(y,data_predict)) #whole data set
-print(sklearn.metrics.r2_score(y[cut_point:,:],data_predict[cut_point:,:])) #only predicted data
-
-#Predcted and actual value
-
-print(y[cut_point:,:],data_predict[cut_point:,:])
+plt.plot(predictions)
+plt.plot(y[cut_point:,:], lw = 5)
+plt.show()
