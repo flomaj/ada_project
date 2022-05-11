7a6ad31057a265208ec94cf27fc6d3febdf1cc33
flomaj <100148022+flomaj@users.noreply.github.com>
2022-05-11 13:21:26 +0200

    Update stationarity.py

--- stationarity.py
+++ stationarity.py
@@ -25,12 +25,17 @@ for i in range(len(data_return.columns)):
     
 #Regarding, the daily returns, we can notice the COVID crash in March 2020.It does not seem to have a trend or seasonal effects, which is a good news for stationarity.
 
-#2nd : Statistical approach = dividing dataset in 4 and check mean/variance. Ideally, each "sub-dataset" has the same mean and the same variance.
+#2nd : Statistical approach = dividing dataset in 4 and check mean/variance. Ideally, each "sub-dataset" has the same mean and the same variance. 
+#We will do this for each variable.
 
 for i in range(len(data_return.columns) - 1):
     data = data_return.values[i]
-    split = round(len(X) / 4)
-    data_1, data_2, data_3, data_4 = data[0:split], data[split:(2*split)], data[(2*split):(3*split)], data[split:]
+    
+    #Split the data in 4 "sub-datasets"
+    split = round(len(data) / 4)
+    data_1, data_2, data_3, data_4 = data[0:split], data[split:(2*split)], data[(2*split):(3*split)], data[split:] 
+    
+    #Compute the mean and the variance of each "sub-dataset".
     mean1, mean2, mean3, mean4 = data_1.mean(), data_2.mean(), data_3.mean(), data_4.mean()
     var1, var2, var3, var4 = data_1.var(), data_2.var(), data_3.var(), data_4.var()
     print("Mean & Variance of", data_return.columns[i])
@@ -46,8 +51,5 @@ for i in range(len(data_return.columns)):
     variables = data_return.values[i]
     result = adfuller(variables)
     print("Results of the test for :", data_return.columns[i])
-    print('ADF Statistic: %f' % result[0])
     print('p-value: %f' % result[1])
-    print('Critical Values:')
-    for key, value in result[4].items():
-        print('\t%s: %.3f' % (key, value))
+    print(40*"-")
